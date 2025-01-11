71282ea0e5a411c1ca5ec45591a88e725adfc871
Bogdan <vujic.bogdan@gmail.com>
2025-01-11 12:02:36 +0100

    Added notification icons next to cart icons, when new products are added to cart
    This feature was added before, but now when the cart icon is moved to header the feature had to be redone also

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectCartProductsCount } from '../state/selectors/cart.selector';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  cartItemCount$!: Observable<number>;
+
+  constructor(private store: Store) {
+    this.cartItemCount$ = this.store.select(selectCartProductsCount);
+  }
+}
--- src/app/state/selectors/cart.selector.ts
+++ src/app/state/selectors/cart.selector.ts
@@ -2,7 +2,13 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { CartState } from '../reducers/cart.reducer';
 
 export const selectCart = createFeatureSelector<CartState>('cart');
+
 export const selectCartProducts = createSelector(
   selectCart,
   (cart: CartState) => cart.products
+);
+
+export const selectCartProductsCount = createSelector(
+  selectCartProducts,
+  (products) => products.length
 );
